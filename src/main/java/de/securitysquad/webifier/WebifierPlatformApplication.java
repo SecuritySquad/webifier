0a9706ef1ca45a5ae9e72809768ffb5d6120be67
samuel-p <samuel.philipp@t-online.de>
2016-11-18 10:53:13 +0100

    configuration for jar file fixed

--- src/main/java/de/securitysquad/webifier/WebifierPlatformApplication.java
+++ src/main/java/de/securitysquad/webifier/WebifierPlatformApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by samuel on 25.10.16.
  */
 @SpringBootApplication
 @ComponentScan("de.securitysquad.webifier")
+@PropertySource("classpath:application.properties")
 public class WebifierPlatformApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(WebifierPlatformApplication.class, args);
