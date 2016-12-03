664f25ce950d1aeeff2b5caf87a8181dbdcd5a5e
samuel-p <samuel.philipp@t-online.de>
2016-12-03 14:04:11 +0100

    install file updated

--- install.sh
+++ install.sh
@@ -2,10 +2,17 @@
 
 cd ..
 
+ls | grep -v "webifier-platform|persistent" | xargs rm -rf
+
 git clone https://github.com/SecuritySquad/webifier-tester.git
 cd webifier-tester
 sh install.sh
 cd ..
+
+if [ -e persistent/application.extension ]
+then
+    cat persistent/application.extension >> webifier-platform/src/main/resources/application.properties
+fi
 
 cd webifier-platform
 ./gradlew :buildAll
@@ -18,6 +25,10 @@ cp ../webifier-platform/build/libs/webifier-platform-all-*.jar .
 JAR=$(ls| grep 'webifier\-platform\-all\-.*\.jar')
 
 cat > start.sh << EOF
+killall java
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
 java -jar ${JAR} > output.log 2>&1 &
 EOF
 
