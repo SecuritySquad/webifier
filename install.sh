ebff87250d45244cd38a1fae5272ed742cb5bc0b
samuel-p <samuel.philipp@t-online.de>
2017-02-18 23:17:38 +0100

    minor changes

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ cd webifier-tester
 sh install.sh
 cd ..
 
-if [ -e persistent/platform-application.extension ]
+if [ -e persistent/application-platform.extension ]
 then
-    cat persistent/platform-application.extension >> webifier-platform/src/main/resources/application.properties
+    cat persistent/application-platform.extension >> webifier-platform/src/main/resources/application.properties
 fi
 
 cd webifier-platform
@@ -30,7 +30,7 @@ killall webifier-plat
 docker stop \$(docker ps -a -q)
 docker rm \$(docker ps -a -q)
 
-LD_PRELOAD=../persistent/libprocname.so PROCNAME=webifier-plat java -jar ${JAR} > output.log 2>&1 &
+LD_PRELOAD=../persistent/libprocname.so PROCNAME=webifier-plat java -jar ${JAR} > output-platform.log 2>&1 &
 EOF
 
 chmod +x start-platform.sh
